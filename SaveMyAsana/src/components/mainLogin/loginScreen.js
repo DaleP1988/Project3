7f5caef5d43143984f68e95c7d6ea661092e2711
Justin Quinonez <justinqnns@gmail.com>
2018-11-14 21:17:08 -0500

    login update

--- SaveMyAsana/src/components/mainLogin/loginScreen.js
+++ SaveMyAsana/src/components/mainLogin/loginScreen.js
@@ -3,28 +3,41 @@ import { Row, Input, Button } from "react-materialize";
 
 
 class FormLogin extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+    login(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+        this.setState({ [name] : value});
+        // this.setState({
+        //     email: event.target.value,
+        //     password: event.target.value
+        // })
+    }
+        render() {
+            console.log('email ', this.state.email)
+            console.log('password', this.state.password)
         return (
             <div className="App card-panel amber lighten-5">
-<Row>
-    <Input type="email" label="E-mail" s={32} />
-    <Input type="password" label="Password" s={32} />
-    <Button waves='light'>Submit</Button>
-</Row>
+            {/* <h1>Her is a compoent</h1> */}
+                <Row>
+                    <Input type="email" label="E-mail" s={32} name="email" value={this.state.email} onChange={this.login} />
+                    <Input type="password" label="Password" s={32} name="password" value={this.state.password} onChange={this.login}/>
+                    <Button waves='light' onClick={this.handleClick}>Submit</Button>
+                </Row>
 
 
-        </div>
+            </div>
         )
     }
 }
 
-function UserProfile(props) {
-    return (
-      <div className="user-profile">
-        <img src={props.userProfile.picture} />
-        <p>{props.userProfile.name}</p>
-      </div>
-    );
-  }
 
 export default FormLogin;
