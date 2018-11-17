ad252f4d5264bbd79a57a4ef888747b3b43fb99b
Garnet <garnet.dianthus@gmail.com>
2018-11-17 10:13:38 -0800

    adds

--- SaveMyAsana/src/components/Grid/Row.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// This Row component lets us use a materialize row without having to think about class names
-export const Row = ({children}) => (
-  <div className={`row`}>
-    {children}
-  </div>
-);
