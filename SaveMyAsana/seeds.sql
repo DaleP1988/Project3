5baa019b51b9d4996e7f1b82b38735c9fe671e8e
Justin Quinonez <justinqnns@gmail.com>
2018-11-26 19:16:05 -0500

    seeds

---
+++ SaveMyAsana/seeds.sql
@@ -0,0 +1,214 @@
+--///////////////////////////////////////
+--//////JUSTIN PLEASE COMPLETE///////////
+--///////////////////////////////////////
+
+
+-- PLEASE COMPLETE REMAINING rows as I have indicated below.
+-- follow the two examples below (Sarah and Mandy are done correctly)
+-- just leave the "q1" as is. I will make sure to select the right corresponding item
+--
+
+
+INSERT INTO instructorPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4, q5, q6, goodie)
+values
+    ("Sarah", "Smith", "123@gmail.com", "(555) 555-5555", "Los Angeles, CA", "bliss.jpg", "q1", "Hi yogis! My name is Sarah Smith and I have been teaching for 15 years. I completed my training at YogaWorks here in Manhattan.", "I took my first yoga class ever at the request of a friend who was a long time follower of Maty Ezraty. Maty was a guru in the LA yoga scene at the time. I knew I wanted what she had. I wanted to find it for myself and share it with others.", "I love watching my yogis grow and live their lives. I love witnessing the breakthroughs and the getting through the day-to-day on the yogic path.", "All that you need you already have. Let the world be your teacher.", "www.yogamat.com");
+
+
+INSERT INTO instructorPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4, q5, q6, goodie)
+values
+    ("Mandy", "O'Share", "m.share@gmail.com", "(562) 299-5555", "Los Angeles, CA", "sunshine.jpg", "q1", "Hey everyone! My name is Mandy O’Share and I want to help you get in touch with your inner self.", "With my intermediate hatha yoga class, you will learn the postures and techniques that have been passed down to me from my family who are also professional yoga instructors. These techniques have been rooted into my being and I am happy to instill them into any developing yogee. ", "The moment that I realized that I wanted to teach was when I was in college and I had some friends who were stressed out over finals. They needed an outlet and I offered my skills. I found that my teachings were able to keep them cool, calm, and collected.", "If you asked me why I teach it would be because I enjoy helping people find themselves through my sessions. ", "With awareness and breath you can achieve anything you set your mind to", "https://www.manduka.com/cork-block-cork-brown-o-s.html");
+
+
+
+---the below need to be completed
+
+INSERT INTO instructorPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4, q5, q6, goodie)
+values
+    ("Sage", "Sunny", "Sunnysage@gmail.com", "(555) 555-5555", "Dallas, TX", "bliss.jpg","q1", "Hello I am Sage. I am a recent yoga client turned instructor. I loved the lessons that I learned from my teachers in the Midwest that I wanted to bring them out to the big City.", "My experiences have been through the teachings of other instructors and through yoga literature from around the world.  My specialities are in iyengar and yoga sculpt.", "I believe that having a strong core can lead to a strong life.", "I have ample time to dedicate to my students. ", "I hope to learn as much from you as you do from me.", "www.yogamat.com");
+
+
+
+-- Hello I am Sage. I am a recent yoga client turned instructor. 
+--I loved the lessons that I learned from my teachers in the Midwest that I wanted to bring them out to the big City. 
+--My experiences have been through the teachings of other instructors and through yoga literature from around the world.  My specialities are in iyengar and yoga sculpt. 
+--I believe that having a strong core can lead to a strong life. 
+--I have ample time to dedicate to my students and hope to learn as much from you as you do from me. 
+
+
+
+INSERT INTO instructorPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4, q5, q6, goodie)
+values
+    ("Estaban", "Geinz", "eGeinz@gmail.com", "(347) 555-5555", "New York, NY", "bliss.jpg", "q1", "Ciao, I am Estaban Geinz. Originally I am from Sicily, but you can catch me theses days in the city of New York. I am your future instructor if you are seeking assistance from an advanced core focused and hot yoga instructor.", "I learned my various teaching styles while backpacking through Europe and learning from others for over 10 years.", "I have developed my own signature style while learning and can assure you that after one of my sessions you will want to book me for the rest of your life. ", "I love sharing my teaching styles with anyone who is willing to dedicate the time to learn. If you have the effort to learn then I have the talent to teach you.", "Make the days count!", "www.yogamat.com");
+
+
+
+-- Ciao, I am Estaban Geinz. Originally I am from Sicily, but you can catch me theses days in the city of New York. 
+--I am your future instructor if you are seeking assistance from an advanced core focused and hot yoga instructor. 
+--I learned my various teaching styles while backpacking through Europe and learning from others for over 10 years. 
+--I have developed my own signature style while learning and can assure you that after one of my sessions you will want to book me for the rest of your life. 
+--I love sharing my teaching styles with anyone who is willing to dedicate the time to learn. If you have the effort to learn then I have the talent to teach you. 
+--Make the days count!
+
+
+
+INSERT INTO instructorPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4, q5, q6, goodie)
+values
+    ("Matt", "Bluth", "mBluth@aol.com", "(555) 555-5555", "Los Angeles, CA", "bliss.jpg", "q1", "Hi I’m Matt Bluth. I have been a yoga instructor for the past 5 years. I have taught one on one sessions and many medium sized groups of about 10 clients per session. I love serving as a restorative instructor. ", "When I was young I was in a bad cycling accident that damaged my ability to bike again. After a few years of restorative yoga I was able to make strides and ride my bicycle again.", "I saw that yoga had the power to positively change my life and I share it forward.", "The journey begins with acceptance.", " Signing up with me will make you see that you can be restored to a better self.", "www.yogamat.com");
+
+
+
+
+-- Hi I’m Matt Bluth. I have been a yoga instructor for the past 5 years. 
+--I have taught one on one sessions and many medium sized groups of about 10 clients per session. I love serving as a restorative instructor. 
+--When I was young I was in a bad cycling accident that damaged my ability to bike again. After a few years of restorative yoga I was able to make strides and ride my bicycle again. 
+--I saw that yoga had the power to positively change my life and I share it forward.
+--The journey begins with acceptance. Signing up with me will make you see that you can be restored to a better self. 
+
+
+
+
+
+INSERT INTO instructorPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4, q5, q6, goodie)
+values
+    ("Trina", "Ming", "TrinaM@hotmail.com", "(555) 555-5555", "Los Angeles, CA", "bliss.jpg", "q1", "Greetings! I am Trina Ming. My experiences with yoga have been relatively new. I have taken new approaches of experiencing with vinyasa flow yoga. Feedback from my clients has allowed me to create a structured program that will allow full synchronization of breath with a continuous flow of postures.", "A client came to me at the end of a session and explained how I have helped her find inner peace while dealing with the stress of her work life.", "I keep teaching because I know that I have made a difference in the life of another.", "Teaching is a transformative experience.", "I would encourage any new client to  dive into the waters headfirst and to be brave when trying something new!", "www.yogamat.com");
+
+
+
+-- Greetings! I am Trina Ming. My experiences with yoga have been relatively new. 
+--I have taken new approaches of experiencing with vinyasa flow yoga. Feedback from my clients has allowed me to create a structured program that will allow full synchronization of breath with a continuous flow of postures. 
+--A client came to me at the end of a session and explained how I have helped her find inner peace while dealing with the stress of her work life. 
+--I keep teaching because I know that I have made a difference in the life of another. 
+--I would encourage any new client to  dive into the waters headfirst and to be brave when trying something new!
+
+
+
+INSERT INTO instructorPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4, q5, q6, goodie)
+values
+    ("Steve", "Megale", "123@gmail.com", "(555) 555-5555", "Celevland, OH", "bliss.jpg", "q1", "What’s up guys! My name is Steve Megale. I have been a traveling instructor for 7 years. I have helped clients coast to coast who have commited the time and effort to make a change. I will come to you and help you in intensive one on one iyengar and core yoga.", "My clients have always told me how I make their lives better. They feel energized and renewed.", "This is rewarding within itself and I have seen how my work has empowered my students.", "Teaching to me is a way to keep the art alive", "If you want to improve it starts with you! Make that first step and sign up today", "www.yogamat.com");
+
+
+-- What’s up guys! My name is Steve Megale. 
+--I have been a traveling instructor for 7 years. I have helped clients coast to coast who have commited the time and effort to make a change. I will come to you and help you in intensive one on one iyengar and core yoga. 
+--My clients have always told me how I make their lives better. They feel energized and renewed. 
+--This is rewarding within itself and I have seen how my work has empowered my students. 
+--If you want to improve it starts with you! Make that first step and sign up today. 
+
+
+
+
+
+INSERT INTO instructorPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4, q5, q6, goodie)
+values
+    ("Stacey", "Simon", "stacysim@gmail.com", "(555) 555-5555", "Atlanta, GA", "bliss.jpg", "q1", "Hi All! My name is Stacey and I am an energetic and effective instructor. My teaching styles are restorative for those who have dealt with significant trauma in their lives.", "My led retoratives and guided meditations help my students cultivate space for healing. I have personally helped over a hundred clients over the last few years. The cases that I have seen have varied but the reason for coming to me is always the same: they need help. I am here to offer that help by providing instruction and guidance.", "My courses have people feeling like they have begun to heal from any baggage that was weighing them down.", "Teach is the start to building and understanding a new self", "I encourage anyone who needs help and does not know how to start with me!", "www.yogamat.com");
+
+
+-- Hi All! My name is Stacey and I am an energetic and effective instructor. 
+--My teaching styles are restorative for those who have dealt with significant trauma in their lives. 
+--My led retoratives and guided meditations help my students cultivate space for healing. I have personally helped over a hundred clients over the last few years. The cases that I have seen have varied but the reason for coming to me is always the same: they need help. I am here to offer that help by providing instruction and guidance. 
+--My courses have people feeling like they have begun to heal from any baggage that was weighing them down. 
+--I encourage anyone who needs help and does not know how to start with me! 
+
+
+-- //////END INSTRUCTOR PROFILES DATA///////
+
+
+
+
+--/////////////////////////////////
+--////////CLIENT PROFILES/////////
+--////////////////////////////////
+
+--for the below follow the example
+--just insert the content following the Jane Smith example
+-- make sure to break up the bio content into q3: "ideal practice environment"  q4: "more about yoga journey. "
+-- follow the Jake Falco example, leave "q1" and "q2" as is. 
+
+--CLIENT PROFILE  (to database and to page, make sure there are two methods in controller for bio and contact)
+
+
+INSERT INTO clientPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4)
+values
+    ("Jake", "Falco", "456@gmail.com", "(888) 888-8888", "Los Angeles, CA", "smile.jpg", "q1", "q2", "Hi guys I am looking for an intermediate yoga session with an instructor. The ideal setting would be at a studio where we can practice poses and breathing. ", "I am trying to impress my girlfriend for our anniversary with our own yoga session. She has always wanted to try but never had that extra push. I wanted to see if anyone here would start us off with a good set of sessions.");
+
+
+
+
+
+INSERT INTO clientPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4)
+values
+    ("Jane", "Smith", "456@gmail.com", "(888) 888-8888", "Los Angeles, CA", "smile.jpg", "q1", "q2", "q3", "q4");
+
+
+-- Sarah
+-- Hello! Looking for an instructor, preferably in a class setting. I have just felt super stressed with family and work. I need a good outlet for all of the negative energy in my life and i believe that I am in the right place. 
+
+
+
+INSERT INTO clientPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4)
+values
+    ("Jane", "Smith", "456@gmail.com", "(888) 888-8888", "Los Angeles, CA", "smile.jpg", "q1", "q2", "q3", "q4");
+
+
+-- Jim
+-- Hi everyone I am looking for a yoga class where i can learn to make use of my core. I feel that I can do better by investing in myself and making this change. I am new to the yoga scene and would love some clear instruction.
+
+
+
+
+INSERT INTO clientPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4)
+values
+    ("Andy", "Brenard", "narddog@gmail.com", "(888) 888-8888", "Ithaca, NY", "smile.jpg", "q1", "q2", "Hello Friends! I would like to pick yoga back up. I use to do it lightly for a for years, but now i want to take the next leap and develop my already existing skills. I know some basics but would love to learn more of the different styles of yoga. This includes bikram and hatha yoga styles.", "Let me know if you can help!");
+
+
+-- Andy
+-- Hello Friends! I would like to pick yoga back up. I use to do it lightly for a for years, but now i want to take the next leap and develop my already existing skills. I know some basics but would love to learn more of the different styles of yoga. This includes bikram and hatha yoga styles.
+-- Let me know if you can help!
+
+
+
+
+INSERT INTO clientPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4)
+values
+    ("Pamela", "Halpert", "Phalp@gmail.com", "(888) 888-8888", "Scranton, PA", "smile.jpg", "q1", "q2", "Hi everyone! I was recently released from physical therapy and I still need some assistance to get to 100%.", "I would like a one on one session with an instructor who can make the trip to me. I think that my experience with yoga is limited but I am willing to learn. Looking forward to anyone who is available!");
+
+
+
+-- Pam
+-- Hi everyone! I was recently released from physical therapy and I still need some assistance to get to 100%. I would like a one on one session with an instructor who can make the trip to me. I think that my experience with yoga is limited but I am willing to learn. Looking forward to anyone who is available!
+
+
+INSERT INTO clientPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4)
+values
+    ("Stanley", "Hudson", "sHudson@gmail.com", "(888) 888-8888", "Orlando, FL", "smile.jpg", "q1", "q2", "Hey! I am a new yogi and would love to learn as much as i can in a classroom session.", "If i could get the benefit of some weight loss that would be great! The style that I am interested in is vinyasa flow yoga. Looking forward to meeting you!");
+
+
+-- Stanley
+-- Hey! I am a new yogi and would love to learn as much as i can in a classroom session. If i could get the benefit of some weight loss that would be great! The style that I am interested in is vinyasa flow yoga. Looking forward to meeting you!
+
+
+INSERT INTO clientPro
+    (Firstname, Lastname, email, phone, loc, img, q1, q2, q3, q4)
+values
+-- Hi! I am an intermediate yogee. I am looking for a class with other lovers of the art of yoga. I have traveled a lot and have made great friends in my various classes. I look forward to any classes that welcome new members!
+    ("Angela", "Schrutte", "Amartin@dundermifflin.com", "(888) 888-8888", "Los Angeles, CA", "smile.jpg", "q1", "q2", "Hi! I am an intermediate yogee. I am looking for a class with other lovers of the art of yoga.", "I have traveled a lot and have made great friends in my various classes. I look forward to any classes that welcome new members!");
+
+
+-- Angela
+-- Hi! I am an intermediate yogee. I am looking for a class with other lovers of the art of yoga. I have traveled a lot and have made great friends in my various classes. I look forward to any classes that welcome new members!
+
+
+--/////////////END CLIENT PROS///////////////
